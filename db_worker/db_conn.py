1760e7f37dc9fa10487a3a6d1c260929b30f839c
USER1377 <dr.galkovsky2017@gmail.com>
2022-09-24 19:14:43 +0500

    Перевёл коннект к БД в класс, добавил функцию инициализации БД, при отсутствии необходимой

--- db_worker/db_conn.py
+++ db_worker/db_conn.py
@@ -5,82 +5,50 @@ from yaml.loader import SafeLoader
 
 
 class database:
-    def __init__(self, proxy_type):
+    def __init__(self):
         with open('conn.yml') as f:
             data = yaml.load(f, Loader=SafeLoader)
         self.url = data['db_conn'][0]['url']
         self.db = data['db_conn'][0]['db']
         self.user = data['db_conn'][0]['user']
         self.passwd = data['db_conn'][0]['passwd']
-        self.proxy_type = proxy_type
         self.first_doc = data['db_conn'][0]['fd']
-        logger.info('Database connection established')
+        self.collections = data['db_conn'][0]['collections']
 
-    def connection(self):
+    def connection_to_mongo(self):
+        # Функция создает подключение к БД Mongo.
         connection_string = 'mongodb://' + self.user + ':' + self.passwd + '@' + self.url + '/' + \
                             '?authSource=admin&retryWrites=true&w=majority'
-        print(connection_string)
         client = MongoClient(connection_string)
-        db = client.freedom_gate
-        return db
+        logger.info('Database connection established')
+        return client
 
     def db_init(self):
-        freedom_db = self.connection()[self.db]
+        # Функция инициализирует БД в случае необходимости и создает коллекции.
         from pymongo.errors import DuplicateKeyError
-        try:
-            init_commit = freedom_db.insert_one(self.first_doc)
-            logger.debug('Attention: DB freedom_gate was inserted')
-            return init_commit
-        except DuplicateKeyError:
-            logger.error('It seems that such a database already exists.')
-
-
-def mongo_export(export_dict: dict):
-    client = MongoClient('localhost', 27017)
-    db = client['freedom_gate']
-    collection = db['http']
-    collection.insert_one(export_dict)
-    client.close()
-
-
-def mongo_update(export_dict: dict):
-    client = MongoClient('localhost', 27017)
-    db = client['freedom_gate']
-    collection = db['http']
-    collection.replace_one(export_dict,
-                           {'_id': export_dict['_id'],
-                            'ip': export_dict['ip'],
-                            'port': export_dict['port'],
-                            'location': export_dict['location'],
-                            'timing': export_dict['timing'],
-                            'proxy_type': export_dict['proxy_type'],
-                            'last_check': export_dict['last_check']
-                            })
-    client.close()
-
-
-def hidemy_export():
-    from proxy_finder.hidemy_name_parser import parse_all as hidemy
-    list_of_proxy = hidemy()
-    for string in list_of_proxy:
-        for item in string:
-            item_to_mongo = {
-                '_id': item,
-                'ip': item,
-                'port': string[item][0],
-                'location': string[item][1],
-                'timing': string[item][2],
-                'proxy_type': string[item][3],
-                'last_check': string[item][5]
-            }
-            from pymongo.errors import DuplicateKeyError
+        freedom_db = self.connection_to_mongo()[self.db]
+        for collection in self.collections:
+            collection_init = freedom_db[collection]
             try:
-                mongo_export(item_to_mongo)
-                logger.info('Try to push dictionary to MongoDB, wait...')
+                collection_init.insert_one(self.first_doc)
+                logger.debug(f'Attention: Collection {collection} was inserted in freedom_gate database')
             except DuplicateKeyError:
-                mongo_update(item_to_mongo)
-                logger.info('It seems that dictionary in MongoDB yet, update document, wait...')
+                logger.error(f'It seems that such a collection {collection} already exists.')
 
-
-dab = database('http')
-print(dab.db_init())
+    def mongo_export(self, export_dict: dict, proxy_type: str):
+        # Функция принимает словарь с данными из парсера и строковое значение типа прокси.
+        # Проверяет в БД наличие таких же записей, если запись есть - проверяет статус прокси
+        # (если в нём были изменения - обновляет), если записи нет - добавляет запись.
+        from pymongo.errors import DuplicateKeyError
+        freedom_db = self.connection_to_mongo()[self.db]
+        try:
+            freedom_db[proxy_type].insert_one(export_dict)
+            logger.info('Document append successful')
+        except DuplicateKeyError:
+            logger.info('It seems that such a document already exists')
+            status_proxy = freedom_db[proxy_type].find_one({'_id': export_dict['_id']})['status']
+            if export_dict['status'] == status_proxy:
+                logger.info('The document does not need to be updated')
+            else:
+                freedom_db[proxy_type].replace_one({'_id': export_dict['_id']}, export_dict, upsert=False)
+                logger.info('Document update')
